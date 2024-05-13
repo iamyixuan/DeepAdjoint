762995eb4ee6141fe8aaa4297e82597cbdbc63fc
Yixuan Sun <yixuan.sun@Yixuans-MacBook-Pro.local>
2024-05-12 21:04:42 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="deep_adjoint",  # Replace with your package's name
+    version="0.1",  # Replace with your package's version
+    packages=find_packages(),  # Automatically find all packages in the project
+)
